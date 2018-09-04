3e0181616d9965bbee4f3c1ff21cadfc5da358d8
Daniel Diaz Medrano <danieldiaz129@gmail.com>
2018-09-04 13:02:22 -0700

    Insertion Sort Added

--- Algorithms/Bubble-Sort.js
+++ Algorithms/Bubble-Sort.js
@@ -1,10 +1,10 @@
-function bubblesort(data) {
+const bubblesort = (data) => {
 	let swapped = true;
 	while(swapped) {
 		swapped = false;
 		for (let i = 0; i < data.length; i++) {
 			if (data[i] > data[i+1]) {
-				let temp = data[i];
+				const temp = data[i];
 				data[i] = data[i+1];
 				data[i+1] = temp;
 				swapped = true;
